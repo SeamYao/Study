6ebd4bab9723a2d2dd5e2b5fb67156728efcaa21
SeamYao <83122877+SeamYao@users.noreply.github.com>
2023-10-08 17:05:50 +0800

    Update KNN.py

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -3,12 +3,14 @@ import pandas as pd
 import math
 df = pd.read_csv('iris.csv',header=None)
 
-#給出一個數據
+#給出一個數據#從這裏更改數據
 new_data = [5.7,3.8,1.7,0.3]
 
 #初始距離
 distance = 200
-type = ''
+label = ''
+
+
 for i in range(len(df)):
     set_distance = 0
     for j in range(4):
@@ -18,6 +20,8 @@ for i in range(len(df)):
     commect_distance = abs(math.sqrt(set_distance))
     print(commect_distance)
     if distance > commect_distance:
+        #更新當前最短距離
         distance = commect_distance
-        type = df.iloc[i][4]
-print(type)
+        #給出label
+        label = df.iloc[i][4]
+print(label)
